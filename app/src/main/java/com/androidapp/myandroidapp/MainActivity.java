c9e55df474f048c6580423af2dd3336518811488
kabue-moringa <joseph.wangari@student.moringaschool.com>
2021-07-04 15:32:36 +0300

    add changes

--- app/src/main/java/com/androidapp/myandroidapp/MainActivity.java
+++ app/src/main/java/com/androidapp/myandroidapp/MainActivity.java
@@ -3,12 +3,24 @@ package com.androidapp.myandroidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindBreweryCompanyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFindBreweryCompanyButton = (Button) findViewById(R.id.findBreweryCompanyButton);
+        mFindBreweryCompanyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //do something
+            }
+        });
+        mFindBreweryCompanyButton = (Button)findViewById(R.id.findBreweryCompanyButton);
+
     }
 }
